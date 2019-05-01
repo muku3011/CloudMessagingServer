135dfdc495f1f16dd6dae8efa6cd576df4febd26
Mukesh Joshi <mukesh.bciit@gmail.com>
2019-05-01 14:51:35 +0200

    Fixed: https://github.com/muku3011/CloudMessagingUi/issues/1

---
+++ src/main/kotlin/com/notifire/server/filter/WebConfig.kt
@@ -0,0 +1,15 @@
+package com.notifire.server.filter
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(ctx: CorsRegistry) {
+        ctx.addMapping("/***")
+                .allowedMethods("OPTIONS", "GET", "PUT", "POST", "DELETE")
+                .allowedOrigins("http://localhost:4200", "https://cloud-messaging-ui.herokuapp.com/")
+                .allowedHeaders("*")
+    }
+}
