6736ac79ba456a4c5b4e5fa20afbc6f6da4945c7
Mukesh Joshi <mukesh.bciit@gmail.com>
2021-05-28 23:54:45 +0200

    Minor changes

--- src/main/kotlin/com/notifire/server/model/Server.kt
+++ src/main/kotlin/com/notifire/server/model/Server.kt
@@ -2,6 +2,7 @@ package com.notifire.server.model
 
 import io.swagger.annotations.ApiModelProperty
 import org.hibernate.validator.constraints.URL
+import org.hibernate.validator.constraints.UniqueElements
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.validation.constraints.NotBlank
@@ -10,14 +11,20 @@ import javax.validation.constraints.NotNull
 @Entity(name = "SERVER")
 data class Server(
 
-        @Id
-        @NotNull(message = "Null value not acceptable for server url")
-        @NotBlank(message = "Empty value not acceptable for server url")
-        @URL(message = "Invalid server url")
-        @ApiModelProperty(position = 1, required = true, value = "Url of the server which will be used to send message", example = "www.google.com")
-        val serverUrl: String,
+    @Id
+    @NotNull(message = "Null value not acceptable for server url")
+    @NotBlank(message = "Empty value not acceptable for server url")
+    @URL(message = "Invalid server url")
+    @ApiModelProperty(
+        position = 1,
+        required = true,
+        value = "Url of the server which will be used to send message",
+        example = "www.google.com"
+    )
+    val serverUrl: String,
 
-        @ApiModelProperty(position = 2, required = true, value = "Server key (secure)", example = "")
-        //@Convert(converter = EncryptDecrypt::class)
-        val serverKey: String
+    @UniqueElements
+    @ApiModelProperty(position = 2, required = true, value = "Server key (secure)", example = "")
+    //@Convert(converter = EncryptDecrypt::class)
+    val serverKey: String
 )
--- src/main/kotlin/com/notifire/server/model/User.kt
+++ src/main/kotlin/com/notifire/server/model/User.kt
@@ -1,17 +1,24 @@
 package com.notifire.server.model
 
 import io.swagger.annotations.ApiModelProperty
+import org.hibernate.validator.constraints.UniqueElements
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @Entity(name = "USER")
 data class User(
 
-        @Id
-        @ApiModelProperty(position = 1, required = true, value = "User to whom message need to be delivered", example = "Mukesh")
-        val userName: String,
+    @Id
+    @ApiModelProperty(
+        position = 1,
+        required = true,
+        value = "User to whom message need to be delivered",
+        example = "Mukesh"
+    )
+    val userName: String,
 
-        @ApiModelProperty(position = 2, required = true, value = "User token (secure)", example = "")
-        //@Convert(converter = EncryptDecrypt::class)
-        val userToken: String
+    @UniqueElements
+    @ApiModelProperty(position = 2, required = true, value = "User token (secure)", example = "")
+    //@Convert(converter = EncryptDecrypt::class)
+    val userToken: String
 )
